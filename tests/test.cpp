3c7ff933547d4bcff08786b395671b3a10155ba3
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-02-10 16:29:03 +0300

    OK

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,7 +8,15 @@ TEST(Example, EmptyTest) {
     EXPECT_TRUE(true);
 }
 TEST(First, True) {
-
+  Stack<int> MySteck;
+  int t=2;
+  MySteck.push(1);
+  MySteck.push(t);
+  EXPECT_EQ(MySteck.head(), 2);
+  EXPECT_EQ(MySteck.pop(), 2);
+  EXPECT_EQ(MySteck.head(), 1);
+  EXPECT_EQ(MySteck.pop(), 1);
+  EXPECT_EQ(MySteck.head(), nullptr);
   EXPECT_FALSE(std::is_copy_constructible<Stack<int>>::value);
   EXPECT_FALSE(std::is_copy_assignable<Stack<int>>::value);
 }
